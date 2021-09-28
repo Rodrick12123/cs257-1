c653bd1593a54dfb4f395929280efe8fb1984b0c
Thea Traw <trawt@carleton.edu>
2021-09-27 22:14:05 -0500

    fixed some indentation

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -32,7 +32,7 @@ class BooksDataSourceTester(unittest.TestCase):
         whatShouldBeReturned = ['Good Omens,1990,Neil Gaiman (1960-) and Terry Pratchett (1948-2015)']
         self.assertEqual(self.data_source("Gaiman and Pratchett"), whatShouldBeReturned)
 
-def test_a_typoTest(self):
+    def test_a_typoTest(self):
         #should be no cases in which a book appears for this search term
         #could also run on books() or even books_between_years()
         self.assertEqual(self.data_source.authors('f1re'), [])
@@ -61,7 +61,7 @@ def test_a_typoTest(self):
         whatShouldBeReturned = ['Wuthering Heights,1847,Emily Brontë (1818-1848)']
         self.assertEqual(self.data_source("Wu"), whatShouldBeReturned)
 
-def test_t_alphabeticalByTitle(self):
+    def test_t_alphabeticalByTitle(self):
         self.assertEqual(self.data_source.books('time', 'title'), ['Love in the Time of Cholera,1985,Gabriel García Márquez (1927-2014)', 'The Fire Next Time,1963,James Baldwin (1924-1987)', 'Thief of Time,1996,Terry Pratchett (1948-2015)'])
     def test_t_sortByYear(self):
         self.assertEqual(self.data_source.books('time', 'year'), ['Thief of Time,1996,Terry Pratchett (1948-2015)', 'Love in the Time of Cholera,1985,Gabriel García Márquez (1927-2014)', 'The Fire Next Time,1963,James Baldwin (1924-1987)'])
@@ -84,7 +84,7 @@ def test_t_alphabeticalByTitle(self):
         whatShouldBeReturned = ['Leave it to Psmith,1923,Pelham Grenville Wodehouse (1881-1975)','The Fire Next Time,1963,James Baldwin (1924-1987)','Love in the Time of Cholera,1985,Gabriel García Márquez (1927-2014)', 'Mirror Dance,1994,Lois McMaster Bujold (1949-)']
         self.assertEqual(self.data_source("1923", "None"), whatShouldBeReturned)
 
-def test_y_inclusiveAndTieBreaker(self):
+    def test_y_inclusiveAndTieBreaker(self):
         self.assertEqual(self.data_source.books_between_years(2005, 2010), ['1Q84,2009,Haruki Murakami (1949-)', 'All Clear,2010,Connie Willis (1945-)', 'Blackout,2010,Connie Willis (1945-)'])
     def test_y_typoTest(self):
         #would a typo just count as a None? probably yes, in which case:
