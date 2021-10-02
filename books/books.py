02b2b32e61dd51de9975b0c0506de03fc347ad68
Thea Traw <trawt@carleton.edu>
2021-10-02 12:36:24 -0500

    edited books.py

--- books/books.py
+++ books/books.py
@@ -1,1 +1,26 @@
-#this is books.py
+#This is code by Lysander Miller and Thea Traw
+
+import argparse
+import booksdatasource
+
+def get_parsed_arguments():
+    parser = argparse.ArgumentParser(description='Sorts books and authors')
+    parser.add_argument('searchterms', metavar='search', nargs='+', help='search function--put in your search term here')
+    parser.add_argument('--titles', '-t', default='title', help='sort by titles')
+    parser.add_argument('--authors', '-a', default=None,  help='sort by authors')
+    parser.add_argument('--years', '-y', default=None, help='sort by years')
+    parsed_arguments = parser.parse_args()
+    return parsed_arguments
+
+def main():
+    arguments = get_parsed_arguments()
+    for term in arguments.searchterms:
+        term = term.split(" ")
+        
+        if (term == '--titles year' or term == '-t year'):
+            booksdatasource.books(
+        else:
+            print(f'I don\'t know what the {animal} says in {arguments.language}')
+
+if __name__ == '__main__':
+    main()
