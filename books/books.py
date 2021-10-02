a11a6bb10e1b194ce24e3f7a5e96a3c740e0c445
millerr2024 <71241942+millerr2024@users.noreply.github.com>
2021-10-02 16:30:05 -0500

    Update books.py

--- books/books.py
+++ books/books.py
@@ -15,8 +15,10 @@ def get_parsed_arguments():
 def main():
     arguments = get_parsed_arguments()
     print(arguments)
+    print(len(arguments.titles))
     for title in arguments.titles:
         #FIX THIS LATER
+        print("HIOI")
         print(booksdatasource.BooksDataSource.books(title, "year"))
     for author in arguments.authors:
         print(booksdatasource.BooksDataSource.authors(author))
