365b6df4914b7e1bc685c528c47ce217246ba6af
millerr2024 <71241942+millerr2024@users.noreply.github.com>
2021-10-02 16:21:15 -0500

    Update books.py

--- books/books.py
+++ books/books.py
@@ -6,8 +6,7 @@ import booksdatasource
 def get_parsed_arguments():
     parser = argparse.ArgumentParser(description='Sorts books and authors')
     parser.add_argument('searchterms', metavar='search', nargs='+', help='search function--put in your search term here')
-    parser.add_argument('--titles', '-t', '--titles title', '-t title', default='--titles', help='sort by titles')
-    parser.add_argument('--titles year', '-t year', default='--titles', help='sort by titles')
+    parser.add_argument('--titles', '-t', default='--titles', help='sort by titles')
     parser.add_argument('--authors', '-a', default=None,  help='sort by authors')
     parser.add_argument('--years', '-y', default=None, help='sort by years')
     parsed_arguments = parser.parse_args()
