e82ca28ca48de5a09f259e183fde2faeae587a4d
millerr2024 <millerr2@carleton.edu>
2021-10-02 21:47:49 -0500

    changes

--- books/books.py
+++ books/books.py
@@ -7,39 +7,50 @@ import csv
 def get_parsed_arguments():
     parser = argparse.ArgumentParser(description='Sorts books and authors')
     parser.add_argument('searchterms', metavar='search', nargs='+', help='search function--put in your search term here')
-    parser.add_argument('--titles', '-t', nargs='+', default="N", help='sort by titles')
-    parser.add_argument('--titles year', '-t year', nargs='+', default="N", help='sort by titles')
-    parser.add_argument('--titles title', '-t title', nargs='+', default="N", help='sort by titles')
-    parser.add_argument('--authors', '-a',nargs='+', default="N",  help='sort by authors')
-    parser.add_argument('--years', '-y',nargs='+', default="N", help='sort by years')
+    parser.add_argument('--titles', '-t', nargs='+', help='sort by titles')
+    parser.add_argument('--titles year', '-t year', nargs='+', help='sort by titles')
+    parser.add_argument('--titles title', '-t title', nargs='+', help='sort by titles')
+    parser.add_argument('--authors', '-a',nargs='+',  help='sort by authors')
+    parser.add_argument('--years', '-y',nargs='+', help='sort by years')
     parsed_arguments = parser.parse_args()
     return parsed_arguments
 
 def main():
     initializedBooksDataSource = booksdatasource.BooksDataSource("books1.csv")
     arguments = get_parsed_arguments()
-    listOfBooks = []
-    for title in arguments.titles:
-        listOfBooks.append(initializedBooksDataSource.books(title))
-        for book in listOfBooks:
-            print(book.title)
+    if (arguments.titles != None): 
+        for title in arguments.titles:
+            listOfBooks = initializedBooksDataSource.books(title, 'title')
+            for book in listOfBooks:
+                print(book.title)
+    if (arguments.titles year != None):
+        for title in arguments.titles year:
+            listOfBooks = initializedBooksDataSource.books(title, 'year')
+            for book in listOfBooks:
+                print(book.title)
+    if (arguments.titles title != None):
+        for title in argument.titles title:
+            listOfBooks = initializedBooksDataSource.books(title)
+            for book in listOfBooks:
+                print(book.title)
     alreadyPrintedAuthors =[]
-    for author in arguments.authors:
-        if (author == "N"):
-            listOfAuthorsNone = initializedBooksDataSource.authors("None")
-            for noneAuthor in listOfAuthorsNone:
-                print(noneAuthor.surname, noneAuthor.given_name)
-        else:
-            listOfAuthorsWithString = initializedBooksDataSource.authors(author)
-            for authorWithString in listOfAuthorsWithString:
-                if ((authorWithString in alreadyPrintedAuthors) == False):
-                    print(authorWithString.surname, authorWithString.given_name)
-                    alreadyPrintedAuthors.append(authorWithString)
-    if (len(arguments.years) > 2):
-        print("")
+    if (arguments.authors != None):
+        for author in arguments.authors:
+            if (author == "N"):
+                listOfAuthorsNone = initializedBooksDataSource.authors(None)
+                for noneAuthor in listOfAuthorsNone:
+                    print(noneAuthor.surname, noneAuthor.given_name)
+            else:
+                listOfAuthorsWithString = initializedBooksDataSource.authors(author)
+                for authorWithString in listOfAuthorsWithString:
+                    if ((authorWithString in alreadyPrintedAuthors) == False):
+                        print(authorWithString.surname, authorWithString.given_name)
+                        alreadyPrintedAuthors.append(authorWithString)
+    #if (len(arguments.years) > 2):
+        #print("")
        #print(arguments.years)
-    else:
-        print("")
+    #else:
+        #print("")
         #print(booksdatasource.BooksDataSource.books_between_years(arguments.years[0], arguments.years[1]))
 
 if __name__ == '__main__':
