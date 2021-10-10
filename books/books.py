0ed4c6f4354c0583a813f2ba097af0fadf398139
theatraw <90911409+theatraw@users.noreply.github.com>
2021-10-10 18:22:43 -0500

    Update books.py

--- books/books.py
+++ books/books.py
@@ -78,8 +78,8 @@ def handle_author_call():
     
 def handle_years_call():
     if (len(arguments.years) == 0):
-       listOfBooks = initializedBooksDataSource.books_between_years(None, None)
-       for book in listOfBooks:
+        listOfBooks = initializedBooksDataSource.books_between_years(None, None)
+        for book in listOfBooks:
             print(book.title)
         elif (len(arguments.years) > 2):
             print("You've entered too many arguments")
