0fe147d5b4c609bbfa28cd490d77237f7d5990e5
Thea Traw <trawt@carleton.edu>
2021-10-11 09:41:43 -0500

    revised functions and variable names

--- books/books.py
+++ books/books.py
@@ -15,92 +15,100 @@ def get_parsed_arguments():
     return parsed_arguments
 
 def main():
-    initializedBooksDataSource = booksdatasource.BooksDataSource("books1.csv")
+    initialized_books_data_source = booksdatasource.BooksDataSource("books1.csv")
     arguments = get_parsed_arguments()
     
-    if (arguments.moreHelp != 'NoData'):
+    if arguments.moreHelp != 'NoData':
         file = open('usage.txt', 'r')
         contents = file.read()
         print(contents)
-    if (arguments.authors != 'NoData'):
-        handle_author_call(initializedBooksDataSource, arguments)
-    if (arguments.titles != 'NoData'):
-        handle_title_call(initializedBooksDataSource, arguments)
-    if (arguments.years != 'NoData'):
-        handle_years_call(initializedBooksDataSource, arguments)
+    if arguments.authors != 'NoData':
+        handle_author_call(initialized_books_data_source, arguments)
+    if arguments.titles != 'NoData':
+        handle_title_call(initialized_books_data_source, arguments)
+    if arguments.years != 'NoData':
+        handle_years_call(initialized_books_data_source, arguments)
                 
-def handle_title_call(initializedBooksDataSource, arguments):
-    sortedByYear = False
-    listOfBooks = []
-    index = 0
-    if ((len(arguments.titles) == 0)):
-        listOfBooks = initializedBooksDataSource.books(None, 'title')
+def handle_title_call(initialized_books_data_source, arguments):
+
+    #not working atm, look in history to see what went wrong
+    
+    sorted_by_year = False
+    number_of_arguments = 0
+    list_of_books = []
+    #index = 0
+    if len(arguments.titles) == 0:
+        list_of_books = initialized_books_data_source.books(None)
     else:
-        for title in arguments.titles:
-            if (title == 'year'):
-                sortedByYear = True
-                index +=1
-            elif (title == 'title'):
-                index +=1
+        for term in arguments.titles:
+            if term == 'year':
+                sorted_by_year = True
+                number_of_arguments += 1
+            elif term == 'title':
+                number_of_arguments += 1
                 pass
             else:
-                if (sortedByYear == True):
-                    listOfBooks += initializedBooksDataSource.books(title, 'year')
-                else:
-                    listOfBooks += initializedBooksDataSource.books(title, 'title')
-                if ((len(arguments.titles)) == index):
-                    if (sortedByYear == True):
-                        listOfBooks = initializedBooksDataSource.books(None, 'year')
+                if number_of_arguments > 1:
+                    if sorted_by_year == True:
+                        list_of_books += initialized_books_data_source.books(term, 'year')
                     else:
-                        listOfBooks = initializedBooksDataSource.books(None, 'title')
-            if (sortedByYear == False):
-                listOfBooks = sorted(listOfBooks, key=lambda x: (x.title))
+                        list_of_books += initialized_books_data_source.books(term, 'title')
+                else:
+                    if sorted_by_year == True:
+                        list_of_books = initialized_books_data_source.books(None, 'year')
+                    else:
+                        list_of_books = initialized_books_data_source.books(None, 'title')
+            if sorted_by_year == False:
+                list_of_books = sorted(list_of_books, key=lambda x: (x.title))
             else:
-                listOfBooks = sorted(listOfBooks, key=lambda x: (x.publication_year, x.title))
-    listOfBooksAlreadyPrinted = []
-    for book in listOfBooks:
-        if (book not in listOfBooksAlreadyPrinted):
+                list_of_books = sorted(list_of_books, key=lambda x: (x.publication_year, x.title))
+    
+    list_of_books_already_printed = []
+    for book in list_of_books:
+        if (book not in list_of_books_already_printed):
             print(book.title)
-            listOfBooksAlreadyPrinted.append(book)
+            list_of_books_already_printed.append(book)
     
-def handle_author_call(initializedBooksDataSource, arguments):
-    listOfAuthors = []
-    listOfAuthorsAlreadyPrinted = []
+def handle_author_call(initialized_books_data_source, arguments):
+    list_of_authors = []
+    list_of_authors_already_printed = []
     
-    if ((len(arguments.authors)) > 0):
+    if len(arguments.authors) > 0:
         for author in arguments.authors:
-            listOfAuthors += initializedBooksDataSource.authors(author)
-    elif ((len(arguments.authors)) == 0):
-        listOfAuthors = initializedBooksDataSource.authors(None)
-        listOfAuthors = sorted(listOfAuthors, key=lambda x: (x.surname, x.given_name))
-        for author in listOfAuthors:
-            if ((author in listOfAuthorsAlreadyPrinted) == False):
-                print(author.surname+', '+author.given_name)
-                for book in initializedBooksDataSource.books_by_author(author):
-                    print("    "+book.title+' ('+book.publication_year+')')
-                listOfAuthorsAlreadyPrinted.append(author)
+            list_of_authors += initialized_books_data_source.authors(author)
+    elif len(arguments.authors) == 0:
+        list_of_authors = initialized_books_data_source.authors(None)
+        #list_of_authors = sorted(list_of_authors, key=lambda x: (x.surname, x.given_name))
+    for author in list_of_authors:
+        if author not in list_of_authors_already_printed:
+            print(author.surname+', '+author.given_name)
+            for book in initialized_books_data_source.books_by_author(author):
+                print("    "+book.title+' ('+book.publication_year+')')
+            list_of_authors_already_printed.append(author)
     
-def handle_years_call(initializedBooksDataSource, arguments):
-    if (len(arguments.years) == 0):
-        listOfBooks = initializedBooksDataSource.books_between_years(None, None)
-        for book in listOfBooks:
-            print(book.title)
-    elif (len(arguments.years) > 2):
+def handle_years_call(initialized_books_data_source, arguments):
+    
+    if len(arguments.years) == 0:
+        list_of_books = initialized_books_data_source.books_between_years(None, None)
+        #for book in listOfBooks:
+            #print(book.title)
+    elif len(arguments.years) > 2:
         print("You've entered too many arguments")
     else:
-        if (len(arguments.years) == 1):
-            listOfBooks = initializedBooksDataSource.books_between_years(arguments.years[0], None)
+        if len(arguments.years) == 1:
+            list_of_books = initialized_books_data_source.books_between_years(arguments.years[0], None)
         elif (arguments.years[0].lower() == 'none' and arguments.years[1].lower() != 'none'):
-            listOfBooks = initializedBooksDataSource.books_between_years(None, arguments.years[1])
-        elif (arguments.years[1].lower() == 'none' and arguments.years[0].lower() != 'none'):
-            listOfBooks = initializedBooksDataSource.books_between_years(arguments.years[0], None)
+            list_of_books = initialized_books_data_source.books_between_years(None, arguments.years[1])
+        elif (arguments.years[0].lower() != 'none' and arguments.years[1].lower() == 'none'):
+            list_of_books = initialized_books_data_source.books_between_years(arguments.years[0], None)
         elif (arguments.years[0].lower() == 'none' and arguments.years[1].lower() == 'none'):
-            listOfBooks = initializedBooksDataSource.books_between_years(None, None)
+            list_of_books = initialized_books_data_source.books_between_years(None, None)
         else:
-            listOfBooks = initializedBooksDataSource.books_between_years(arguments.years[0], arguments.years[1])
-            for book in listOfBooks:
-                print(book.title)
+            list_of_books = initialized_books_data_source.books_between_years(arguments.years[0], arguments.years[1])
     
+    for book in list_of_books:
+        print(book.title)
+
 
 if __name__ == '__main__':
     main()
