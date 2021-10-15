70ba84eb2dc186fc5f1d8e2d93c4b07ef8d4fb36
Thea Traw <trawt@carleton.edu>
2021-10-14 23:54:08 -0500

    worked on queries

---
+++ olympics/queries.sql
@@ -0,0 +1,33 @@
+#List all the NOCs (National Olympic Committees), in alphabetical order by abbreviation. These entities, by the way, are mostly equivalent to countries. But in some cases, you might find that a portion of a country participated in a particular games (e.g. one guy from Newfoundland in 1904) or some other oddball situation.
+
+SELECT 
+FROM
+WHERE
+AND
+ORDER BY
+
+#List the names of all the athletes from Kenya. If your database design allows it, sort the athletes by last name.
+
+SELECT
+FROM
+WHERE
+AND
+ORDER BY
+
+
+#List all the medals won by Greg Louganis, sorted by year. Include whatever fields in this output that you think appropriate.
+
+SELECT
+FROM
+WHERE
+AND
+ORDER BY
+
+
+#List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
+
+SELECT
+FROM
+WHERE
+AND
+ORDER BY
