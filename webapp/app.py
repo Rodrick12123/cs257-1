16184096173694a87c635303f7a16281feb242c0
Thea Traw <trawt@carleton.edu>
2021-11-15 18:53:41 -0600

    made /help route

--- webapp/app.py
+++ webapp/app.py
@@ -29,6 +29,9 @@ def page3():
 def page4():
     return flask.render_template('OneTeamAllCups.html')
 
+@app.route('/help/')
+def help():
+    return flask.render_template('help.html')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Flask applications')
